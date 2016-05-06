5152275c1f7d58c56f0c24fbce67de378d72a798
Steven Butler <srbutler@gmail.com>
2016-05-05 23:23:13 -0400

    Working full demo with styling.

--- app.py
+++ app.py
@@ -1,38 +1,19 @@
 """Gets a UD def."""
 
-import string
+from collections import OrderedDict
+import re
 
-from flask import Flask, request, render_template
+from flask import Flask, flash, request, render_template
 from lxml import html
 import requests
+from wtforms import Form, TextAreaField
 
 
 app = Flask(__name__)
 
 
-def get_ud_string(word):
-    """Get the top UD definition of a word as a 'word: def' string."""
-
-    url = "http://www.urbandictionary.com/define.php?term=%s" % word
-    page = requests.get(url)
-    tree = html.fromstring(page.content)
-
-    words = tree.xpath('//a[@class="word"]/text()')
-    meanings = tree.xpath('//div[@class="meaning"]/text()')
-    ranks = tree.xpath('//div[@class="ribbon"]/text()')
-
-    # examples = tree.xpath('//div[@class="example"]/text()')
-    # counts = tree.xpath('//div[@class="count"]/text()')
-
-    top_def_str = None
-
-    for tup in zip(words, meanings, ranks):
-
-        if tup[2].strip() == "Top Definition":
-            top_def_str = "{}: {}".format(tup[0].strip().lower(),
-                                          tup[1].strip())
-
-    return top_def_str
+class InputForm(Form):
+    text_input = TextAreaField("")
 
 
 def get_ud_def(word):
@@ -45,7 +26,7 @@ def get_ud_def(word):
     words = tree.xpath('//a[@class="word"]/text()')
     meanings = tree.xpath('//div[@class="meaning"]/text()')
     ranks = tree.xpath('//div[@class="ribbon"]/text()')
-
+    
     top_def = None
 
     for tup in zip(words, meanings, ranks):
@@ -58,7 +39,7 @@ def get_ud_def(word):
 
 def chunk_text(input_text, chunk_size=4):
 
-    exclude = set(string.punctuation)
+    exclude = set('!"#$%&()*+,-./:;<=>?@[\]^_`{|}~')
 
     depuncted_text = ''.join(ch.lower() for ch in input_text if ch not in exclude)
 
@@ -75,17 +56,33 @@ def chunk_text(input_text, chunk_size=4):
 
         candidates.extend(indiv_candidates)
 
-    # print(len(candidates))
-    return set(candidates)
+    candidate_dict = OrderedDict()
+
+    for cand in candidates:
+        candidate_dict[cand] = 0
+
+    return candidate_dict.keys()
 
 
-def filter_stop_words(input_iter, stopwords_file, n_words=100):
+def filter_stop_words(input_iter, stopwords_file, n_words=1000):
 
     with open(stopwords_file, 'r') as f:
         data = f.readlines()
-        data_words = [x.split(' ')[0] for x in data]
+        data_words = [x.split(' ')[0].lower() for x in data]
 
     return [w for w in input_iter if w not in data_words[:n_words]]
+
+
+def check_identity(word, returned_word):
+
+    if word == returned_word:
+        return True
+    elif word == returned_word + "s":
+        return True
+    elif word == returned_word + "es":
+        return True
+    else:
+        return False
 
 
 def evaluate_text(input_text):
@@ -93,52 +90,50 @@ def evaluate_text(input_text):
     wordlist_file = "data/english_wordlist.txt"
 
     check_words = filter_stop_words(chunk_text(input_text), wordlist_file)
-
-    word_dict = {}
+    
+    word_dict = OrderedDict()
     for word in check_words:
         ud_def = get_ud_def(word.lower())
 
         if ud_def is not None:
-            word_dict[ud_def[0]] = ud_def[1]
+            if check_identity(word, ud_def[0]):
+                if len(ud_def[1]) >= 1:
+                    word_dict[ud_def[0]] = ud_def[1]
 
     return word_dict
 
 
-def format_return(input_text):
+def markup_text(input_text, word_defs):
 
-    final_str = ""
+    hl_words = word_defs.keys()
 
-    wordlist_file = "data/english_wordlist.txt"
+    out_text = input_text
 
-    check_words = filter_stop_words(chunk_text(input_text), wordlist_file)
+    for word in hl_words:
 
-    for word in check_words:
-        ud_def_str = get_ud_string(word.lower())
+        pattern = '({})'.format(word)
+        repl = '<em>\g<1></em>'
+        out_text = re.sub(pattern, repl, out_text)
 
-        if ud_def_str is not None:
-            final_str += ud_def_str + "<br><br>"
-
-    return final_str
+    return out_text
 
 
 @app.route('/')
-def my_form():
-    return render_template("input-form.html")
+def get_input():
+    
+    form = InputForm(request.form)
+    return render_template("input-form.html", form=form)
 
 
-@app.route('/', methods=['POST'])
-def my_form_post():
-
-    text = request.form['text']
-
+@app.route('/output', methods=['POST'])
+def show_output():
+    
+    text = request.form.getlist('text_input')[0]
     word_defs = evaluate_text(text)
-    return render_template("output-page.html", word_defs=word_defs)
 
-    # formatted_defs = format_return(text)
-    # return render_template("output-page.html",
-    #                        input_text=text,
-    #                        formatted_input=formatted_defs)
-
+    return render_template("output-page.html",
+                           input_text=text,
+                           word_defs=word_defs)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
